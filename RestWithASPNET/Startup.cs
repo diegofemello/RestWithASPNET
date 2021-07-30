0e8637cab41a223486b65ef3a69b5c46d3b808ed
Diego Mello <diegofemello@gmail.com>
2021-07-30 14:00:52 -0300

    Criada classe pessoa e configuração do banco de dados

--- RestWithASPNET/Startup.cs
+++ RestWithASPNET/Startup.cs
@@ -1,32 +1,45 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using RestWithASPNET.Model.Context;
+using RestWithASPNET.Services.Implementations;
+using Pomelo.EntityFrameworkCore.MySql;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace RestWithASPNET
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public IConfiguration Configuration { get; }
+        public IWebHostEnvironment Environment { get; }
+        public Startup(IConfiguration configuration, IWebHostEnvironment environment)
         {
             Configuration = configuration;
+            Environment = environment;
         }
 
-        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
+
+            var connectionString = Configuration["MySQLConnection:MySQLConnectionString"];
+
+            var serverVersion = new MySqlServerVersion(new Version(8, 0, 25));
+            services.AddDbContext<MySQLContext>(
+            dbContextOptions => dbContextOptions
+                .UseMySql(connectionString, serverVersion)
+                .EnableSensitiveDataLogging() // <-- These two calls are optional but help
+                .EnableDetailedErrors()       // <-- with debugging (remove for production).
+        );
+
+            // Dependency Injection
+            services.AddScoped<IPersonService, PersonServiceImplementation>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

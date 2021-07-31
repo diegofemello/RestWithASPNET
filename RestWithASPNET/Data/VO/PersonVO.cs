0747007c2806d84339354bcb73fb653fb67507a1
Diego Mello <diegofemello@gmail.com>
2021-07-31 01:59:32 -0300

    Value Object

---
+++ RestWithASPNET/Data/VO/PersonVO.cs
@@ -0,0 +1,17 @@
+﻿using RestWithASPNET.Model.Base;
+
+namespace RestWithASPNET.Data.VO
+{
+
+    public class PersonVO : BaseEntity
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+
+        public string Address { get; set; }
+
+        public string Gender { get; set; }
+    }
+}

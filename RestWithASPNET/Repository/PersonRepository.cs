ed7914c0aa56129cd615d4633e156f159b44f8c0
Diego Mello <diegofemello@gmail.com>
2021-08-01 23:58:51 -0300

    Limpeza de comentário

--- RestWithASPNET/Repository/PersonRepository.cs
+++ RestWithASPNET/Repository/PersonRepository.cs
@@ -56,24 +56,6 @@ namespace RestWithASPNET.Repository
             }
             return null;
 
-            //if (!string.IsNullOrWhiteSpace(firstName) && !string.IsNullOrWhiteSpace(lastName))
-            //{
-            //    return _context.Persons.Where(
-            //        p => p.FirstName.Contains(firstName) &&
-            //        p.LastName.Contains(lastName)).ToList();
-            //}
-            //else if (string.IsNullOrWhiteSpace(firstName) && !string.IsNullOrWhiteSpace(lastName))
-            //{
-            //    return _context.Persons.Where(
-            //        p => p.LastName.Contains(lastName)).ToList();
-            //}
-            //else if (!string.IsNullOrWhiteSpace(firstName) && string.IsNullOrWhiteSpace(lastName))
-            //{
-            //    return _context.Persons.Where(
-            //        p => p.FirstName.Contains(firstName)).ToList();
-            //}
-            //return null;
-
         }
     }
 }
